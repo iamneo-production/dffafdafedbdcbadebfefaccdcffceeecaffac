a37ad54a7b6325e336654549132ee93004375b59
Iamneo <techadmin@iamneo.ai>
2023-08-18 12:06:21 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/JDBCUtils.java
+++ springapp/src/main/java/com/examly/springapp/JDBCUtils.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 
 public class JDBCUtils {
 
-	
 	private static final String URL = "jdbc:mysql://localhost:3306/appdb?createDatabaseIfNotExist=true";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "examly";
