a436c88df0350301ca09192347a8ecb9ac3a9117
Iamneo <techadmin@iamneo.ai>
2023-08-22 10:45:50 +0000

    Backup Commit

--- backup.sql
+++ backup.sql
@@ -66,7 +66,7 @@ CREATE TABLE `player` (
   PRIMARY KEY (`id`),
   KEY `FKdvd6ljes11r44igawmpm1mc5s` (`team_id`),
   CONSTRAINT `FKdvd6ljes11r44igawmpm1mc5s` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +75,7 @@ CREATE TABLE `player` (
 
 LOCK TABLES `player` WRITE;
 /*!40000 ALTER TABLE `player` DISABLE KEYS */;
-INSERT INTO `player` VALUES (4,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','',4),(5,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(6,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(7,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(8,2000,10000,'Senior Team',NULL,'Player 1','',8),(9,24,2000,'Junior Team',NULL,'Player2','',9),(10,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(11,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(12,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(13,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(14,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(15,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL);
+INSERT INTO `player` VALUES (4,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','',4),(5,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(6,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(7,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(8,2000,10000,'Senior Team',NULL,'Player 1','',8),(9,24,2000,'Junior Team',NULL,'Player2','',9),(10,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(11,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(12,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(13,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(14,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(15,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL),(16,24,15000,'Seniorteam','Viratdemo@gmail.com','Demo','\0',NULL);
 /*!40000 ALTER TABLE `player` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +91,7 @@ CREATE TABLE `team` (
   `maximum_budget` double NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +100,7 @@ CREATE TABLE `team` (
 
 LOCK TABLES `team` WRITE;
 /*!40000 ALTER TABLE `team` DISABLE KEYS */;
-INSERT INTO `team` VALUES (4,15000,'Demo'),(5,15000,'Demo'),(6,15000,'Demo'),(7,15000,'Demo'),(8,100000,'Player A'),(9,100000,'Team A'),(10,15000,'Demo'),(11,15000,'Demo'),(12,15000,'Demo'),(13,15000,'Demo'),(14,15000,'Demo'),(15,1,'asss'),(16,15000,'Demo');
+INSERT INTO `team` VALUES (4,15000,'Demo'),(5,15000,'Demo'),(6,15000,'Demo'),(7,15000,'Demo'),(8,100000,'Player A'),(9,100000,'Team A'),(10,15000,'Demo'),(11,15000,'Demo'),(12,15000,'Demo'),(13,15000,'Demo'),(14,15000,'Demo'),(15,1,'asss'),(16,15000,'Demo'),(17,15000,'Demo');
 /*!40000 ALTER TABLE `team` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,7 +140,7 @@ CREATE TABLE `user` (
   `role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +149,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'$2a$10$NRaGjMbbksXZrfqzzvUzte9eVww7MeaR6L1AOipdWOK2mJTy.oBDm','ADMIN','admin'),(2,'$2a$10$Ut8bGacxq0Tt8amn12zZHOUM4Um6zrARsE8lgB73PqIIsdMS2t8WC','ADMIN','admin'),(3,'$2a$10$vdkVioYdZ6F8dQ6lvk6EiOrQ1w1umBu20x3SoGPV3a271eRQcUnma','ADMIN','admin'),(4,'$2a$10$c4onFI.At9FqkAL1L.msx.FVYcLa1gYFWumqpQoI86vF7qtztbhO.','ADMIN','admin'),(5,'$2a$10$90ip0IZl/Qe5sZVQ65Q4Oumh1DuxCKTZuTZQ4Zwx2/ut7e9zOQyfy','ORGANIZER','Demo1'),(6,'$2a$10$6FCYwzEVmqr2I70E/kXIXuXCCPqqrlOUHpuQV02mWYkrTkTRiDZO2','ADMIN','admin'),(7,'$2a$10$L5z2lUbKdyBErTXDDdvKc.nnHHcW8k4cPelfytWBRHytgsbVgszJS','ADMIN','admin'),(8,'$2a$10$3l4OX5bMVGkmHqcd35AqBuBuhxcNKkXe4pjd4Mehp5bSt.2kPz6im','ADMIN','admin'),(9,'$2a$10$rFxKV0imN2QDeP/TwCP36eBI5KWvPCTyErokD1Uo4UIMV28lN3gqC','ADMIN','admin'),(10,'$2a$10$nq1vQq.TR6826p5beWpxau9rb2NwAamVFuEEjZ9N.flO4NoqtjSHy','ADMIN','admin'),(11,'$2a$10$.yW8JbMmX.5jVBfUq.6NDedBaw8aixmrevVw/GTlDK/RqRWpF3RF.','ADMIN','admin'),(12,'$2a$10$T1mPO4chIASaQwp.DzVlqemaNTRCLh5g8yqwgAqLHg3a6DVWBCgW2','ADMIN','admin'),(13,'$2a$10$1VFdHr2fnsFjTWH1oXQx7ujb4Ci9WJGpdc5AHNZIoQ6zD/FRur5/.','ADMIN','admin'),(14,'$2a$10$ImacOGxv/8ZaS/2LLzb15OQURoeRsC/yS.ma5eF6L4Y7ZpgXLRrbC','ADMIN','admin'),(15,'$2a$10$ArfO6i.abq8.s9eHJKRooOgOX0SqzBMgxoukY09L5hyW7EHrlHWxa','ADMIN','admin'),(16,'$2a$10$w6pgZ4WnAd8oIQxyQNbkJ.KaY2Dp5beJLTl111iFUmqBOSDgl2n0m','ADMIN','admin');
+INSERT INTO `user` VALUES (1,'$2a$10$NRaGjMbbksXZrfqzzvUzte9eVww7MeaR6L1AOipdWOK2mJTy.oBDm','ADMIN','admin'),(2,'$2a$10$Ut8bGacxq0Tt8amn12zZHOUM4Um6zrARsE8lgB73PqIIsdMS2t8WC','ADMIN','admin'),(3,'$2a$10$vdkVioYdZ6F8dQ6lvk6EiOrQ1w1umBu20x3SoGPV3a271eRQcUnma','ADMIN','admin'),(4,'$2a$10$c4onFI.At9FqkAL1L.msx.FVYcLa1gYFWumqpQoI86vF7qtztbhO.','ADMIN','admin'),(5,'$2a$10$90ip0IZl/Qe5sZVQ65Q4Oumh1DuxCKTZuTZQ4Zwx2/ut7e9zOQyfy','ORGANIZER','Demo1'),(6,'$2a$10$6FCYwzEVmqr2I70E/kXIXuXCCPqqrlOUHpuQV02mWYkrTkTRiDZO2','ADMIN','admin'),(7,'$2a$10$L5z2lUbKdyBErTXDDdvKc.nnHHcW8k4cPelfytWBRHytgsbVgszJS','ADMIN','admin'),(8,'$2a$10$3l4OX5bMVGkmHqcd35AqBuBuhxcNKkXe4pjd4Mehp5bSt.2kPz6im','ADMIN','admin'),(9,'$2a$10$rFxKV0imN2QDeP/TwCP36eBI5KWvPCTyErokD1Uo4UIMV28lN3gqC','ADMIN','admin'),(10,'$2a$10$nq1vQq.TR6826p5beWpxau9rb2NwAamVFuEEjZ9N.flO4NoqtjSHy','ADMIN','admin'),(11,'$2a$10$.yW8JbMmX.5jVBfUq.6NDedBaw8aixmrevVw/GTlDK/RqRWpF3RF.','ADMIN','admin'),(12,'$2a$10$T1mPO4chIASaQwp.DzVlqemaNTRCLh5g8yqwgAqLHg3a6DVWBCgW2','ADMIN','admin'),(13,'$2a$10$1VFdHr2fnsFjTWH1oXQx7ujb4Ci9WJGpdc5AHNZIoQ6zD/FRur5/.','ADMIN','admin'),(14,'$2a$10$ImacOGxv/8ZaS/2LLzb15OQURoeRsC/yS.ma5eF6L4Y7ZpgXLRrbC','ADMIN','admin'),(15,'$2a$10$ArfO6i.abq8.s9eHJKRooOgOX0SqzBMgxoukY09L5hyW7EHrlHWxa','ADMIN','admin'),(16,'$2a$10$w6pgZ4WnAd8oIQxyQNbkJ.KaY2Dp5beJLTl111iFUmqBOSDgl2n0m','ADMIN','admin'),(17,'$2a$10$PEJCE/poKfjyIitG430X9OhOI13J6yvi6IFwRUK6C0CpY4CRgoyjO','ADMIN','admin');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -162,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-22  8:07:07
+-- Dump completed on 2023-08-22 10:45:49
